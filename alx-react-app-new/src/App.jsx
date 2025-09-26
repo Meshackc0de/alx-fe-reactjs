59d3b0c683501ff35a226734d93a9640318ea823
Meshackc0de <meshackjohn420@gmail.com>
2025-09-26 11:50:56 +0300

    fix footer.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
-import Footer from './Footer.jsx'
+import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
 import Counter from './components/Counter.jsx'
 function App() {
