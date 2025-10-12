5c49b39eec6ebc98c9cea239e95924d4dab67e93
Meshackc0de <meshackjohn420@gmail.com>
2025-10-13 01:38:53 +0300

    Fix RegistrationForm

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -16,17 +16,17 @@ function RegistartionForm() {
     return ( 
         <>
         <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='username' value= {username}
+            <input type='text' placeholder='username' value={username}
             onChange={(e) => setUsername(e.target.value)}
-            /> <br/>
+            />
 
             <input type='email' placeholder='email' value={email}
             onChange= {(e) => setEmail(e.target.value) }
-            /><br/>
+            />
 
             <input type='password' placeholder='password' value={password}
             onChange={(e) => setPassword(e.target.value)}
-            /> <br/>
+            />
             <button type='submit'>Register</button>
         </form>
         
